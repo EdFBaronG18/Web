067745d093c8e43466ee33de0255c9da0d05c9b7
Edward-Acer <edwardbaron99@gmail.com>
2019-03-03 14:18:02 -0500

    Inicializar datos formulario.

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -16,7 +16,18 @@ export class UsuarioComponent implements OnInit {
   constructor() {
 
     this.miUsuario = new Usuario;
-    
+    this.miUsuario = {
+        nombre: 'Edward',
+        apellido: 'Baron',
+        pais: 'Colombia',
+        ciudad: 'El Rosal',
+        direccion: 'Calle 12 # 2 - 40 - 42',
+        universidad: 'Politecnico Grancolombiano',
+        titulo: 'Estudiante',
+        correo: 'edbarongutierre@poligran.edu.co',
+        celular: '3202049072',
+        telefono: '123456789'
+    };
    }
 
   ngOnInit() {
