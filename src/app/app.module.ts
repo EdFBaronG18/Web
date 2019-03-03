5bb61fe4a7fe35d65fa25ea6958082e35861c054
Edward-Acer <edwardbaron99@gmail.com>
2019-03-03 18:28:10 -0500

    Ultimos cambios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
